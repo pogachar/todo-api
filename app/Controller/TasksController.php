bb591351bec3442f73f4d685d09d55a34db5e7f8
Jernej Pogačar <pogacar.jernej@gmail.com>
2016-09-05 19:05:49 +0200

    update TasksController

--- app/Controller/TasksController.php
+++ app/Controller/TasksController.php
@@ -5,33 +5,49 @@ App::uses('AppController', 'Controller');
 class TasksController extends AppController {
 
 	public $uses = ['Task'];
-	public $components = ['RequestHandler'];
+	public $components = ['RequestHandler', 'Paginator'];
 	public $layout = null;
 	public $autoRender = false;
 
+	public function beforeFilter()
+	{
+		if ($this->request->method() == 'OPTIONS') {
+			$this->response->header('Access-Control-Allow-Headers', 'Content-Type');
+			$this->response->send();
+			exit;
+		}
+	}
+
+	public function afterFilter()
+	{
+		$this->RequestHandler->respondAs('application/json');
+	}
+
 	public function index()
 	{
-		$tasks = $this->Task->find('all');
+		$this->Paginator->settings = [
+			'limit' => 3,
+			'page' => $this->request->query('page') ?: 1
+		];
 
-		$statusCode = count($tasks) ? 200 : 204;
+		$this->response->statusCode(200);
 
-		$this->RequestHandler->respondAs('application/json');
-		$this->response->statusCode($statusCode);
-		return json_encode($tasks);
+		return $this->response->body(json_encode([
+			'data' => $this->Paginator->paginate(),
+			'pagination' => $this->makePagination()
+		]));
 	}
 
 	public function view($id)
 	{
 		$task = $this->Task->findById($id);
 
-		$this->RequestHandler->respondAs('application/json');
-
 		if(!count($task)) {
 			return $this->response->statusCode(404);
 		}
 
 		$this->response->statusCode(200);
-		return json_encode($task['Task']);
+		return $this->response->body(json_encode($task));
 	}
 
 	public function add()
@@ -40,14 +56,13 @@ class TasksController extends AppController {
 
 		if(!$this->Task->validates()) {
 			$this->response->statusCode(400);
-			return json_encode($this->Task->invalidFields());
+			return $this->response->body(json_encode($this->Task->invalidFields()));
 		}
 
 		$task = $this->Task->save($this->request->data);
 
-		$this->RequestHandler->respondAs('application/json');
 		$this->response->statusCode(201);
-		return json_encode($task['Task']);
+		return $this->response->body(json_encode($task));
 	}
 
 	public function edit($id)
@@ -57,22 +72,43 @@ class TasksController extends AppController {
 
 		if(!$this->Task->validates()) {
 			$this->response->statusCode(400);
-			return json_encode($this->Task->invalidFields());
+			return $this->response->body(json_encode($this->Task->invalidFields()));
 		}
 
-		$task = $this->Task->save($this->request->data);
+		$data = $this->request->data;
+		if(!isset($data['completed_at']) || $data['completed_at'] == null) {
+			$data['completed_at'] = '0000-00-00 00:00:00';
+		}
 
-		$this->RequestHandler->respondAs('application/json');
+		$task = $this->Task->save($data);
+
 		$this->response->statusCode(200);
-		return json_encode($task['Task']);
+		return $this->response->body(json_encode($task));
 	}
 
 	public function delete($id)
 	{
 		$this->Task->delete($id);
 
-		$this->RequestHandler->respondAs('application/json');
 		return $this->response->statusCode(204);
+	}
+	/**
+	 * @return array
+	 */
+	private function makePagination()
+	{
+		$perPage = 3;
+		$total = $this->Task->find('count');
+		$currentPage = (int)$this->request->query('page') ?: 1;
+
+		return [
+			'total' => $total,
+			'per_page' => $perPage,
+			'current_page' => $currentPage,
+			'last_page' => ceil($total / $perPage),
+			'from' => ($currentPage - 1) * $perPage + 1,
+			'to' => $currentPage * $perPage
+		];
 	}
 
 }
